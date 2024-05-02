befa68b6f410c5ea8d98b4519f7800d47a9981e8
TrDucChinh <chinhdevptit@gmail.com>
2024-05-02 22:35:32 +0700

    first commit

---
+++ desktop/src/com/pgjs2bplantsimulator/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.pgjs2bplantsimulator;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.pgjs2bplantsimulator.S2BPlantSimulator;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("PGJ_S2B_Plant_Simulator");
+		new Lwjgl3Application(new S2BPlantSimulator(), config);
+	}
+}
