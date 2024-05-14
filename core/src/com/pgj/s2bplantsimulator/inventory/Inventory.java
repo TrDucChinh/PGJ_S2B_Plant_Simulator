9ace0919a8f7ed3a9271ad070f5ddf4ddc19ae24
MemoGlow <88959500+MemoGlow@users.noreply.github.com>
2024-05-14 19:39:59 +0700

    feat: create plant, plantulti, inventory

---
+++ core/src/com/pgj/s2bplantsimulator/inventory/Inventory.java
@@ -0,0 +1,34 @@
+package com.pgj.s2bplantsimulator.inventory;
+
+import com.pgj.s2bplantsimulator.model.Player;
+import com.pgj.s2bplantsimulator.ultis.PlantUltis;
+
+import java.util.ArrayList;
+
+public class Inventory {
+    private ArrayList<Item> items;
+    Item currentItem;
+    Player holder;
+    public void setCurrentItem(Item item){
+        this.currentItem = item;
+    }
+    public Inventory(Player holder){
+        items = new ArrayList<>();
+        this.holder = holder;
+
+    }
+
+    public ArrayList<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(ArrayList<Item> items) {
+        this.items = items;
+    }
+    public void loadItems(PlantUltis plantUltis){
+        for(int i = 0; i < 10; i++){
+            Plant plant = plantUltis.createPlant(0);
+            items.add(plant);
+        }
+    }
+}
